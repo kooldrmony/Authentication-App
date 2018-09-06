3d44bc01b8ddeb1fc19e2b7edb6a7464951fb495
kooldrmony <derrickreyes.23@gmail.com>
2018-09-06 07:54:11 -0700

    added src components common and index.js directory and file

---
+++ src/components/LoginIn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import LoginIn from './common/Button';
+
+const LoginIn = () => {
+
+	return (
+
+		<View>
+			<Button>Login In</Button>
+		</View>
+
+		);
+};
+
+export default LoginIn;
