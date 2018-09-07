b64418a48ea638f1e3cc1c93b540fa5bdec9938c
kooldrmony <derrickreyes.23@gmail.com>
2018-09-06 20:46:17 -0700

    added Input.js component

---
+++ src/components/common/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+const Input = ({ label }) => {
+
+	return (
+		<View>
+			<Text>{label}</Text>
+		</View>
+		);
+};
+
+export default Input;
